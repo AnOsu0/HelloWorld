baafcda4ed563de8712df445e76effeb1225bafb
AnOsu0 <as.ania02@gmail.com>
2022-02-08 19:17:09 +0100

    08.02.2022

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("witaj świecie");
         System.out.println("No cześć");
+        System.out.println("wam też");
     }
 }
